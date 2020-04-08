c57f84872cfe6a1dba31fc0287c0ee8ced9eeadd
MaksimkinEvgenij <61985452+MaksimkinEvgenij@users.noreply.github.com>
2020-04-08 14:22:51 +0300

    Движение шаров

--- ex_1.py
+++ ex_1.py
@@ -11,15 +11,27 @@ colors = ['red','orange','yellow','green','blue']
 
 score = 0
 
+balls =[]
+
 def new_ball():
     """ Создание очередного шара. """
     global x,y,r
-    canv.delete(ALL)
+    #canv.delete(ALL)
     x = rnd(100,700)
     y = rnd(100,500)
     r = rnd(30,50)
-    canv.create_oval(x-r,y-r,x+r,y+r,fill = choice(colors), width=0)
+    balls.append(canv.create_oval(x-r,y-r,x+r,y+r,fill = choice(colors), width=0))
+    
+
+    
     root.after(1000,new_ball)
+
+
+def move_balls():
+    """ Движение шаров. """
+    for b in balls:
+        canv.move(b, +10, +10)
+    root.after(100,move_balls)
 
 
 def click(event):
@@ -31,5 +43,6 @@ def click(event):
         
 
 new_ball()
+move_balls()
 canv.bind('<Button-1>', click)
 mainloop()
