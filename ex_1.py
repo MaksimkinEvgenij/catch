2f78792ea3736623cfa9991cb7011e3fe919d1b3
MaksimkinEvgenij <61985452+MaksimkinEvgenij@users.noreply.github.com>
2020-04-08 14:46:08 +0300

    Добавил удаление шаров при выборе их; плюс мелкие исправления

--- ex_1.py
+++ ex_1.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from random import randrange as rnd, choice
 import time
+
 root = Tk()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -13,11 +14,10 @@ colors = ['red','orange','yellow','green','blue']
 
 score = 0
 
-balls =[]
+balls =[]   # список созданных шаров
 
 def new_ball():
     """ Создание очередного шара. """
-    global x,y,r
     #canv.delete(ALL)
     x = rnd(100,700)
     y = rnd(100,500)
@@ -32,28 +32,35 @@ def new_ball():
 def move_balls():
     """ Движение шаров. """
     for i in balls:
-        i[1] += i[3]    # изменение x на dx
-        i[2] += i[4]    # изменение y на dy
-        canv.move(i[0], i[3], i[4])
+        if i:
+            i[1] += i[3]    # изменение x на dx
+            i[2] += i[4]    # изменение y на dy
+            canv.move(i[0], i[3], i[4])
     reflection_from_walls()
     root.after(100,move_balls)
 
 
 def reflection_from_walls():
     """ Отражение от стен шаров. """
-    # изменени dx и dy, если шар доходит до размеров окна
+    # изменение dx и dy, если шар доходит до размеров окна
     for i in balls:
-        if i[1] + i[5] > SCREEN_WIDTH or i[1] - i[5] < 0:
-            i[3] *= -1
-        if i[2] + i[5] > SCREEN_HEIGHT or i[2] - i[5] < 0:
-            i[4] *= -1    
+        if i:
+            if i[1] + i[5] > SCREEN_WIDTH or i[1] - i[5] < 0:
+                i[3] *= -1
+            if i[2] + i[5] > SCREEN_HEIGHT or i[2] - i[5] < 0:
+                i[4] *= -1    
 
 def click(event):
     """ Проверка попадания щелчком мыши в шар. """
     global score
-    if ((x - event.x)**2 + (y - event.y)**2)**(1/2) <= r:
-        print('Попал')
-        score += 1
+
+    for i in range(len(balls)):
+        if balls[i]:
+            if ((balls[i][1] - event.x)**2 + (balls[i][2] - event.y)**2)**(1/2) <= balls[i][5]:
+                print('Попал')
+                canv.delete(balls[i][0])
+                balls[i] = None
+                score += 1
         
 
 new_ball()
