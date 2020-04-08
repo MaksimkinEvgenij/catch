a1d88e2dea2774238a18649b00ffbc6c8fe51808
MaksimkinEvgenij <61985452+MaksimkinEvgenij@users.noreply.github.com>
2020-04-08 13:15:15 +0300

    Подсчет очков

--- ex_1.py
+++ ex_1.py
@@ -8,9 +8,11 @@ canv = Canvas(root,bg='white')
 canv.pack(fill=BOTH,expand=1)
 
 colors = ['red','orange','yellow','green','blue']
+
+score = 0
+
 def new_ball():
     """ Создание очередного шара. """
-
     global x,y,r
     canv.delete(ALL)
     x = rnd(100,700)
@@ -21,13 +23,12 @@ def new_ball():
 
 
 def click(event):
-
     """ Проверка попадания щелчком мыши в шар. """
+    global score
     if ((x - event.x)**2 + (y - event.y)**2)**(1/2) <= r:
         print('Попал')
-
-    print(x,y,r)
-
+        score += 1
+        
 
 new_ball()
 canv.bind('<Button-1>', click)
